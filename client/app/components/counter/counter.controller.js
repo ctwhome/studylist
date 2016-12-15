67ca3cba5579e14562238f5b5d988d818f17626d
javiergarciagonzalez <javier@thispage.amsterdam>
2016-12-15 12:52:02 +0100

    Fixed pause button on counter-controller

--- client/app/components/counter/counter.controller.js
+++ client/app/components/counter/counter.controller.js
@@ -4,28 +4,9 @@ class CounterController {
     'ngInject'
 
     this.$interval = $interval;
-
-    // THIS WORKS!!!!
-    //
-    // let seconds = 10;
-    // this.interval = this.$interval(() => {
-    //   seconds--;
-    //   this.counter = seconds;
-    // }, 1000, seconds).then(()=>{
-    //   console.log(444444);
-    // })
-    //
-    //
-    // setTimeout(()=>{
-    //   this.$interval.cancel(this.interval)
-    // }, 3000);
-
   }
 
-
   $onInit() {
-
-    // console.log(this.$interval);
 
     this.DEFAULT_COUNTER = 600;
     this.counter = this.DEFAULT_COUNTER;
@@ -39,13 +20,7 @@ class CounterController {
     this.interval = this.$interval(() => {
       seconds--;
       this.counter = seconds;
-    }, 1000, seconds).then(() => {
-
-      // buffers automatically when created
-      var snd = new Audio("assets/guitar.wav");
-      snd.play();
-      // this.counter = this.DEFAULT_COUNTER;
-    });
+    }, 1000, seconds);
   }
 
   toggleTime() {
@@ -57,21 +32,23 @@ class CounterController {
   }
 
   pause() {
-
-    // console.info("angular.isDefined(this.interval) ", angular.isDefined(this.interval));
-    // this.$interval.cancel(this.interval);
-
     if (angular.isDefined(this.interval)) {
       console.log("the promies ", this.$interval.cancel(this.interval));
-      this.$interval.cancel(this.interval)
-      this.interval = undefined;
+      this.$interval.cancel(this.interval);
+      this.playAudio();
     }
   }
 
+  playAudio () {
+
+      // buffers automatically when created
+      let snd = new Audio("assets/guitar.wav");
+      snd.play();
+  }
 
   reset() {
     if (angular.isDefined(this.interval)) {
-      this.$interval.cancel(this.interval)
+      this.interval.cancel(this.interval)
     }
     this.counter = this.DEFAULT_COUNTER;
   }
