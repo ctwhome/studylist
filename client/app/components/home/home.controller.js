224a00b02fcf915a99dcf2679df3bce1efefdf3b
Jesus <ctw@ctwhome.com>
2016-12-16 13:49:35 +0100

    working with the list

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -10,7 +10,7 @@ class HomeController {
 
   }
 
-  $onInit(){
+  $onInit() {
 
     this.name = 'home';
 
@@ -35,49 +35,60 @@ class HomeController {
         picture: '//icons.iconarchive.com/icons/graphicloads/flat-finance/256/person-icon.png',
         lists: [
           {
-            name: 'Study',
+            id:0,
+            name: "Study",
             timers: [
-              {
-                name: 'Timer 1',
-                time: '1500'
-              },
-              {
-                name: 'Timer 2',
-                time: '1500'
-              },
-              {
-                name: 'Timer 3',
-                time: '1500'
-              }
+              {name: "task 1", desc: "A kind of a long desctription", time: 600},
+              {name: "task 2", desc: "A kind of a long desctription", time: 200},
+              {name: "task 3", desc: "A kind of a long desctription", time: 300},
+              {name: "task 4", desc: "A kind of a long desctription", time: 300},
+              {name: "task 5", desc: "A kind of a long desctription", time: 400}
+            ]
+          },
+
+          {
+            id:1,
+            name: "Piano",
+            timers: [
+              {name: "Scales", desc: "A kind of a long desctription", time: 200},
+              {name: "Technique", desc: "A kind of a long desctription", time: 300},
+              {name: "Song", desc: "A kind of a long desctription", time: 300},
             ]
           },
           {
-            name: 'Piano',
+            id:2,
+            name: "Language",
             timers: [
-              {
-                name: 'Timer 4',
-                time: '1500'
-              },
-              {
-                name: 'Timer 5',
-                time: '1500'
-              }
+              {name: "Vocabulary", desc: "A kind of a long desctription", time: 600},
+              {name: "Grammatica", desc: "A kind of a long desctription", time: 300},
+              {name: "Reading", desc: "A kind of a long desctription", time: 300},
+              {name: "Listening", desc: "A kind of a long desctription", time: 300}
             ]
           }
-        ]
+
+        ],
+        jsonlist: {
+          0: [
+            {name: "task 1", desc: "A kind of a long desctription", time: 600},
+            {name: "task 2", desc: "A kind of a long desctription", time: 200},
+            {name: "task 3", desc: "A kind of a long desctription", time: 300},
+            {name: "task 4", desc: "A kind of a long desctription", time: 300},
+            {name: "task 5", desc: "A kind of a long desctription", time: 400}
+          ],
+          1: [
+            {name: "task 1", desc: "A kind of a long desctription", time: 600},
+            {name: "task 2", desc: "A kind of a long desctription", time: 200},
+            {name: "task 3", desc: "A kind of a long desctription", time: 300},
+            {name: "task 4", desc: "A kind of a long desctription", time: 300},
+            {name: "task 5", desc: "A kind of a long desctription", time: 400}
+          ]
+
+        }
       };
 
 
     this.activeList = this.user.lists[0].name;
 
-
-    this.list = [
-      {name: "task 1", desc:"A kind of a long desctription", time: 600},
-      {name: "task 2", desc:"A kind of a long desctription", time: 600},
-      {name: "task 3", desc:"A kind of a long desctription", time: 600},
-      {name: "task 4", desc:"A kind of a long desctription", time: 600},
-      {name: "task 5", desc:"A kind of a long desctription", time: 600}
-    ]
   }
 
   delete(index) {
@@ -89,21 +100,21 @@ class HomeController {
   }
 
   addDialog(ev) {
-      this.mdDialog.show({
-         controller: AddDialogController,
-        controllerAs:'$ctrl',
-        template: require('./add-dialog.html'),
-        parent: angular.element(document.body),
-        targetEvent: ev,
-        clickOutsideToClose:true,
-      })
-        .then((task)=> {
-          // $scope.status = 'You said the information was "' + answer + '".';
-          console.log(task);
-     this.add(task);
-        }, function() {
-          // $scope.status = 'You cancelled the dialog.';
-        });
+    this.mdDialog.show({
+      controller: AddDialogController,
+      controllerAs: '$ctrl',
+      template: require('./add-dialog.html'),
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose: true,
+    })
+      .then((task) => {
+        // $scope.status = 'You said the information was "' + answer + '".';
+        console.log(task);
+        this.add(task);
+      }, function () {
+        // $scope.status = 'You cancelled the dialog.';
+      });
 
   }
 
